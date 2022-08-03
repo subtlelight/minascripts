1a5eaf17c7e8891a8254f64f6c692f336a7e84a3
tony <ultra.kultura@gmail.com>
2022-08-03 20:30:11 +0300

    generatekeys

--- node-install.sh
+++ node-install.sh
@@ -3,19 +3,46 @@
 echo
 #echo "set MINA VERSION https://docs.minaprotocol.com/en/getting-started "
 #read -p "mina-mainnet=: " minaversion
-echo
-echo "set MINA KEY PASSWORD"
-read -p "MINA KEY PASSWORD: " minakeypass
-echo
-echo "set COINBASE RECEIVER ADDRESS"
-read -p "COINBASE RECEIVER ADDRESS: " coinbasereceiver
-echo
-echo "set private key ~/key/my-wallet https://docs.minaprotocol.com/en/using-mina/keypair "
-read -p "~/key/my-wallet: " walletkey
-echo
-echo "set public key key/my-wallet.pub https://docs.minaprotocol.com/en/using-mina/keypair "
+echo "set public key key/my-wallet.pub if not set NEW KEYS WILL BE GENERATED!!! "
 read -p "key/my-wallet: " walletpubkey
+
+if [ -z "$walletpubkey" ]; then
+  echo '$walletpubkey is not set GENERATING NEW '
+  echo "deb [trusted=yes] http://packages.o1test.net $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/mina.list
+  sudo apt-get update && sudo apt-get install mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
+  mkdir ~/keys
+  chmod 700 ~/keys
+  mina-generate-keypair --privkey-path ~/key/my-wallet
+  chmod 600 ~/key/my-wallet
+  else
+   echo
+   echo "set private key ~/key/my-wallet https://docs.minaprotocol.com/en/using-mina/keypair "
+   read -p "~/key/my-wallet: " walletkey
+fi
 echo
+echo
+echo "set MINA KEY PASSWORD AGAIN"
+   read -p "MINA KEY PASSWORD AGAIN: " minakeypass
+echo
+echo
+echo "set COINBASE RECEIVER ADDRESS "
+read -p "COINBASE RECEIVER ADDRESS: " coinbasereceiver
+if [ -z "$coinbasereceiver" ]; then
+  echo 'coinbasereceiver is not set'
+  echo "deb [trusted=yes] http://packages.o1test.net $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/mina.list
+  sudo apt-get update && sudo apt-get install mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
+  mkdir ~/coinrec
+  chmod 700 ~/coinrec
+  mina-generate-keypair --privkey-path ~/coinrec/my-wallet
+  chmod 600 ~/coinrec/my-wallet
+  coinbasereceiver="cat keys/my-wallet.pub"
+  else
+   echo
+   echo "set COINBASE RECEIVER ADDRESS"
+   read -p "COINBASE RECEIVER ADDRESS: " coinbasereceiver
+   echo
+fi
+
 username="whoami"
 
 sudo echo "deb [trusted=yes] http://packages.o1test.net stretch stable" | sudo tee /etc/apt/sources.list.d/mina.list
@@ -29,11 +56,15 @@ LOG_LEVEL=Info
 FILE_LOG_LEVEL=Debug
 EXTRA_FLAGS=" --block-producer-key /home/minadmin/keys/my-wallet --uptime-submitter-key /home/minadmin/keys/my-wallet --uptime-url https://uptime-backend.minaprotocol.com/v1/submit --limited-graphql-port 3095 --minimum-block-reward 684 --coinbase-receiver '"$coinbasereceiver"' "' > .mina-env
 
-mkdir ~/keys
-chmod 700 ~/keys
-echo "$walletkey" > ~/keys/my-wallet
-chmod 600 ~/keys/my-wallet
-echo "$walletpubkey" > ~/keys/my-wallet.pub
+if [ -z "$walletpubkey" ]; then
+      echo
+   else
+     mkdir ~/keys
+     chmod 700 ~/keys
+     echo "$walletkey" > ~/keys/my-wallet
+     chmod 600 ~/keys/my-wallet
+     echo "$walletpubkey" > ~/keys/my-wallet.pub
+fi
 
 sudo systemctl daemon-reload && systemctl --user daemon-reload && systemctl --user stop mina && systemctl --user restart mina
 
