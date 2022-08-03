bd05cd7af29e5bd8b9945f2b9b76ea4190099f3d
tony <ultra.kultura@gmail.com>
2022-08-03 21:12:45 +0300

    fix

--- node-install.sh
+++ node-install.sh
@@ -9,15 +9,15 @@ read -p "key/my-wallet: " walletpubkey
 if [ -z "$walletpubkey" ]; then
   echo '$walletpubkey is not set GENERATING NEW '
   echo "deb [trusted=yes] http://packages.o1test.net $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/mina.list
-  sudo apt-get update && sudo apt-get install mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
+  sudo apt-get update && sudo apt-get install -y mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
   mkdir ~/keys
   chmod 700 ~/keys
-  mina-generate-keypair --privkey-path ~/key/my-wallet
-  chmod 600 ~/key/my-wallet
+  mina-generate-keypair --privkey-path ~/keys/my-wallet
+  chmod 600 ~/keys/my-wallet
   else
    echo
-   echo "set private key ~/key/my-wallet https://docs.minaprotocol.com/en/using-mina/keypair "
-   read -p "~/key/my-wallet: " walletkey
+   echo "set private key ~/keys/my-wallet https://docs.minaprotocol.com/en/using-mina/keypair "
+   read -p "~/keys/my-wallet: " walletkey
 fi
 echo
 echo
@@ -30,12 +30,12 @@ read -p "COINBASE RECEIVER ADDRESS: " coinbasereceiver
 if [ -z "$coinbasereceiver" ]; then
   echo 'coinbasereceiver is not set'
   echo "deb [trusted=yes] http://packages.o1test.net $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/mina.list
-  sudo apt-get update && sudo apt-get install mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
+  sudo apt-get update && sudo apt-get install -y mina-generate-keypair=1.3.0-9b0369c libjemalloc-dev
   mkdir ~/coinrec
   chmod 700 ~/coinrec
   mina-generate-keypair --privkey-path ~/coinrec/my-wallet
   chmod 600 ~/coinrec/my-wallet
-  coinbasereceiver="cat keys/my-wallet.pub"
+  coinbasereceiver=$(<~/coinrec/my-wallet.pub)
   else
    echo
    echo "set COINBASE RECEIVER ADDRESS"
@@ -54,7 +54,7 @@ UPTIME_PRIVKEY_PASS="'"$minakeypass"'"
 MINA_PRIVKEY_PASS="'"$minakeypass"'"
 LOG_LEVEL=Info
 FILE_LOG_LEVEL=Debug
-EXTRA_FLAGS=" --block-producer-key /home/minadmin/keys/my-wallet --uptime-submitter-key /home/minadmin/keys/my-wallet --uptime-url https://uptime-backend.minaprotocol.com/v1/submit --limited-graphql-port 3095 --minimum-block-reward 684 --coinbase-receiver '"$coinbasereceiver"' "' > .mina-env
+EXTRA_FLAGS=" --block-producer-key /home/minadmin/keys/my-wallet --uptime-submitter-key /home/minadmin/keys/my-wallet --uptime-url https://uptime-backend.minaprotocol.com/v1/submit --limited-graphql-port 3095 --minimum-block-reward 684 --coinbase-receiver '"${!coinbasereceiver}"' "' > .mina-env
 
 if [ -z "$walletpubkey" ]; then
       echo
