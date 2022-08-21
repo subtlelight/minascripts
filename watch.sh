9f4da5fba316ad9328be7e8dc4042b0a5d44c706
tony <ultra.kultura@gmail.com>
2022-08-21 18:11:50 +0300

    update

--- watch.sh
+++ watch.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
+status=$(/usr/local/bin/mina client status | grep -e 'Local uptime' -e 'Sync status' -e 'Block height' -e 'Max observed block height' -e 'Max observed unvalidated block height')
+sidecar=$(sudo journalctl -u mina-bp-stats-sidecar.service | tail -n 7 | grep Finished | awk '{print $1,$2,$3,$6,$9}')
+
+echo "Status:"$'\n'$'\n'"$status"$'\n'$'\n'"Sidecar Stats:"$'\n'$'\n'"$sidecar"
+echo
 echo "Uptime service - sent : $(journalctl --user -u mina -S "today" | grep 'Sent block with state' | wc -l), failed: $(journalctl --user -u mina -S "today" | grep 'After 8 attempts, failed' | wc -l), Errors : $(journalctl --user -u mina -S "today" | grep 'uptime' | grep 'Error' | wc -l)"
