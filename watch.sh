8d50d7eccf835a16e8f2044b49346cfcc2eefcac
tony <ultra.kultura@gmail.com>
2022-08-21 18:40:21 +0300

    add ext ip4

--- watch.sh
+++ watch.sh
@@ -1,8 +1,10 @@
 #!bin/bash
-
+ip4=$(curl -s 'http://2ip.ru')
 status=$(/usr/local/bin/mina client status | grep -e 'Local uptime' -e 'Sync status' -e 'Block height' -e 'Max observed block height' -e 'Max observed unvalidated block height')
 sidecar=$(sudo journalctl -u mina-bp-stats-sidecar.service | tail -n 7 | grep Finished | awk '{print $1,$2,$3,$6,$9}')
 
+echo "ip4: $ip4"
+echo
 echo "Status:"$'\n'$'\n'"$status"$'\n'$'\n'"Sidecar Stats:"$'\n'$'\n'"$sidecar"
 echo
 echo "Uptime service - sent : $(journalctl --user -u mina -S "today" | grep 'Sent block with state' | wc -l), failed: $(journalctl --user -u mina -S "today" | grep 'After 8 attempts, failed' | wc -l), Errors : $(journalctl --user -u mina -S "today" | grep 'uptime' | grep 'Error' | wc -l)"
