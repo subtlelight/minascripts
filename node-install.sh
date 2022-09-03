0738ce5d1170707c8068ec90c88cd19b0d014c61
tony <ultra.kultura@gmail.com>
2022-09-03 20:10:10 +0300

    add libraries

--- node-install.sh
+++ node-install.sh
@@ -9,7 +9,7 @@ echo
 echo
 echo
 echo "SET public key key/my-wallet.pub if not set NEW KEYS WILL BE GENERATED!!! "
-read -p "key/my-wallet: " walletpubkey
+read -p "key/my-wallet.pub : " walletpubkey
 
 if [ -z "$walletpubkey" ]; then
   echo '$walletpubkey is not set GENERATING NEW '
@@ -62,6 +62,38 @@ if [ -z "$walletpubkey" ]; then
      chmod 600 ~/keys/my-wallet
      echo "$walletpubkey" > ~/keys/my-wallet.pub
 fi
+
+YELLOW="\033[33m"
+GREEN="\033[32m"
+
+echo "---------------"
+echo -e "$YELLOW Downloading libraries.\033[0m"
+echo "---------------"
+echo
+cd
+mkdir -p libs
+cd libs 
+wget http://mirrors.kernel.org/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-8_amd64.deb
+wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/j/jemalloc/libjemalloc1_3.6.0-11_amd64.deb
+wget http://mirrors.edge.kernel.org/ubuntu/pool/main/p/procps/libprocps6_3.3.12-3ubuntu1_amd64.deb
+
+echo "---------------"
+echo -e "$YELLOW Installing libraries.\033[0m"
+echo "---------------"
+
+sudo dpkg -i *.deb
+cd
+
+echo "---------------"
+echo -e "$YELLOW Deleting all packages.\033[0m"
+echo "---------------"
+
+rm -rf libs
+
+echo "---------------"
+echo -e "$GREEN Libraries successfully installed.\033[0m"
+echo "---------------"
+
 
 systemctl --user daemon-reload && systemctl --user start mina && systemctl --user enable mina && sudo loginctl enable-linger
 
