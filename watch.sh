1a9516516af15c50ac21214dca49a61cc7172700
subtlelight <93600568+subtlelight@users.noreply.github.com>
2023-10-15 15:37:45 +0300

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-ip4=$(curl -s 'http://2ip.ru')
+ip4=$(curl -s 'http://ifconfig.me')
 status=$(/usr/local/bin/mina client status | grep -e 'Local uptime' -e 'Sync status' -e 'Block height' -e 'Max observed block height' -e 'Max observed unvalidated block height' -e 'Next block will be produced in' -e 'External IP')
 sidecar=$(sudo journalctl -u mina-bp-stats-sidecar.service | tail -n 7 | grep Finished | awk '{print $1,$2,$3,$6,$9}')
 
